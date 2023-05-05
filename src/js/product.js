f57b70809f570cda6c79ae64abda91c161acc58a
Levi Jones <37312664+levi-wj@users.noreply.github.com>
2023-05-04 19:06:50 -0600

    Updated all links and fixed build. (#9)

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,4 @@
-import { setLocalStorage, getLocalStorage } from './utils.mjs';
-import { findProductById } from './productData.mjs';
+import { getParam } from './utils.mjs';
+import productDetails from './productDetails.mjs';
 
-
-function addProductToCart(product) {
-  let cartItems = getLocalStorage('so-cart') || [];
-  
-  cartItems.push(product);
-  setLocalStorage('so-cart', cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+productDetails(getParam('id'), '.product-detail');
--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,22 +1,38 @@
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
 import { findProductById } from './productData.mjs';
-import { getParam } from './utils.mjs';
 
-async function createProductDetails(id) {
+function addProductToCart(product) {
+  let cartItems = getLocalStorage('so-cart') || [];
+  
+  cartItems.push(product);
+  setLocalStorage('so-cart', cartItems);
+}
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+
+async function productDetailsTemplate(id) {
     const product = await findProductById(id);
-    return `<h3 id="productName">${product.Name}</h3>
-    <h2 class="divider" id="productNameWithoutBrand">${product.NameWithoutBrand}</h2>
-    <img id="productImage" class="divider" src="${product.Image}" alt="${product.DescriptionHtmlSimple}" />
-    <p class="product-card__price" id="productFinalPrice">${product.FinalPrice}</p>
-    <p class="product__color" id="productColorName">${product.Colors[0].ColorName}</p>
-    <p class="product__description" id="productDescriptionHtmlSimple">${product.DescriptionHtmlSimple}</p>
-    <div class="product-detail__add">
-    <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-    </div>`
+    return `
+        <h3 id="productName">${product.Name}</h3>
+        <h2 class="divider" id="productNameWithoutBrand">${product.NameWithoutBrand}</h2>
+        <img id="productImage" class="divider" src="${product.Image}" alt="${product.DescriptionHtmlSimple}" />
+        <p class="product-card__price" id="productFinalPrice">${product.FinalPrice}</p>
+        <p class="product__color" id="productColorName">${product.Colors[0].ColorName}</p>
+        <p class="product__description" id="productDescriptionHtmlSimple">${product.DescriptionHtmlSimple}</p>
+        <div class="product-detail__add">
+            <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+        </div>`
 }
 
-async function writeToHtml() {
-    const productDetails = await createProductDetails(getParam('id'));
-    document.querySelector('.product-detail').innerHTML = productDetails;
-}
+export default async function productDetails(productId, selector) {
+    const productHTML = await productDetailsTemplate(productId);
+    document.querySelector(selector).innerHTML = productHTML;
 
-window.addEventListener('load', () => {writeToHtml()});
+    // add listener to Add to Cart button
+    document
+        .getElementById('addToCart')
+        .addEventListener('click', addToCartHandler);
+}
