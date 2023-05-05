f51f475d27ba1540fd920d0d8127b5695115cc66
Levi Jones <leviwill.j@gmail.com>
2023-05-05 11:57:42 -0600

    Added delete buttons to cart items

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,16 +1,32 @@
-import { getLocalStorage } from './utils.mjs';
+import { getLocalStorage, setClick } from './utils.mjs';
+import { removeProductFromCart } from './productDetails.mjs';
+
+function setupOnclicks(productList) {
+  // Set up onclicks for trash buttons
+  productList.querySelectorAll('.fa-trash').forEach(btn => {
+    setClick(btn, (e) => {
+      removeProductFromCart(e.target.dataset.id);
+      renderCartContents();
+    });
+  });
+}
 
 function renderCartContents() {
   const cartItems = getLocalStorage('so-cart') || [];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  const productList = document.querySelector('.product-list');
   let totalCost = 0; // initialize totalCost to zero
+
+  productList.innerHTML = htmlItems.join('');
   cartItems.forEach((item) => (totalCost += item.FinalPrice));
-  document.querySelector('.product-list').innerHTML = htmlItems.join('');
+
   document.querySelector('.cart-footer').innerHTML = `Total: $${totalCost}`;
   if (cartItems.length > 0) {
     const cartFooterElement = document.querySelector('.cart-footer');
     cartFooterElement.classList.remove('hide');
   }
+
+  setupOnclicks(productList);
 }
 
 function cartItemTemplate(item) {
@@ -27,6 +43,7 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <i class="fa fa-trash icon-btn cart-card__trash" data-id="${item.Id}"></i>
 </li>`;
 
   return newItem;
