d440833cdbb7eac158dd9ada1db5b259afb4d238
emmaohn <joh20123@byui.edu>
2023-05-16 18:47:34 -0600

    fixed product index logo, add and remove animations, sticky header, utility animate function

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,16 +1,22 @@
 import { setLocalStorage, getLocalStorage } from './utils.mjs';
 import { displayBubble } from './cartBubble.mjs';
+import { startAnimation } from './utils.mjs';
 
 export function removeProductFromCart(id) {
   let cartItems = getLocalStorage('so-cart') || [];
   setLocalStorage('so-cart', cartItems.filter(item => item.Id !== id));
+
+  const backpack = document.querySelector('.cart');
+  startAnimation(backpack, 'shake');
   displayBubble();
 }
 
 export function addProductToCart(product) {
   let cartItems = getLocalStorage('so-cart') || [];
-  
   cartItems.push(product);
   setLocalStorage('so-cart', cartItems);
+  
+  const backpack = document.querySelector('.cart');
+  startAnimation(backpack, 'jump');
   displayBubble();
 }
