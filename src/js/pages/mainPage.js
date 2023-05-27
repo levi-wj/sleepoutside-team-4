43375868145c83fac22cca550c01fb08a5d0130a
Justyn Mencl <92409211+JM3N5LICE@users.noreply.github.com>
2023-05-26 19:19:55 -0600

    Created newletter subscription form and button

--- src/js/pages/mainPage.js
+++ src/js/pages/mainPage.js
@@ -2,3 +2,12 @@
 import { renderHeaderFooter } from '../utils.mjs';
 
 renderHeaderFooter();
+
+function showConfirmation() {
+    document.getElementById('confirmation-message').style.display = 'block';
+  }
+  
+  document.getElementById('newsletter-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    showConfirmation();
+  });
