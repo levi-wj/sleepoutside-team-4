6778c5c54e45732bd055ef57a340fc924e6ad5d6
Justyn Mencl <92409211+JM3N5LICE@users.noreply.github.com>
2023-06-02 16:52:53 -0600

    Banner at the bottom

--- src/js/pages/mainPage.js
+++ src/js/pages/mainPage.js
@@ -11,3 +11,18 @@ function showConfirmation() {
     event.preventDefault(); // Prevent form submission
     showConfirmation();
   });
+
+// Close button functionality
+document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('banner').style.display = 'none';
+  });
+
+  document.getElementById('register-btn').addEventListener('click', function() {
+    // Display a thank you message
+    document.getElementById('banner-content').innerHTML = '<h2>Thank You!</h2><p>Thank you for registering!</p>';
+
+    // Close the banner/modal after a brief delay (e.g., 2 seconds)
+    setTimeout(function() {
+      document.getElementById('banner').style.display = 'none';
+    }, 2000);
+  });
