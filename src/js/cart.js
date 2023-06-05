fa7dcc6423e25825d872e1ed2ed8b1c5d385a83f
YongLeeCode <yonghwan311@gmail.com>
2023-06-05 11:23:48 -0600

    duplicate and quantity

--- src/js/cart.js
+++ src/js/cart.js
@@ -13,23 +13,71 @@ export function removeProductFromCart(id) {
 
 export function addProductToCart(product) {
   let cartItems = getLocalStorage('so-cart') || [];
-  const backpack = document.querySelector('.cart');
 
-  cartItems.push(product);
-  setLocalStorage('so-cart', cartItems);
+
+  product.currentQuantity = 1;
+  let match = false;
+
+  //duplicate
+  if(cartItems.length === 0){
+    cartItems.push(product);
+    setLocalStorage('so-cart', cartItems);
+  } else{
+    for(var i = 0; i < cartItems.length; i++){
+      if(product.Id == cartItems[i].Id){
+        cartItems[i].currentQuantity++;
+        setLocalStorage('so-cart', cartItems);
+        match = true;
+        const backpack = document.querySelector('.cart');
+        startAnimation(backpack, 'jump');
+
+        break;
+      } else{
+        // eslint-disable-next-line no-console
+        console.log(`already matched`)
+      }
+    }
+  }
+  if (match == false){
+    cartItems.push(product);
+    setLocalStorage('so-cart', cartItems);
+
+    const backpack = document.querySelector('.cart');
+    startAnimation(backpack, 'jump');
+  }
+  
   cartCount.set(getTotalCartItems());
-  startAnimation(backpack, 'jump');
 }
 
 export function getCartTotal() {
   const items = getLocalStorage('so-cart') || [];
   let totalCost = 0;
 
-  items.forEach((item) => (totalCost += item.FinalPrice));
+  items.forEach((item) => (totalCost += quantityItem(item)));
   return totalCost;
 }
 
 export function getTotalCartItems() {
   const items = getLocalStorage('so-cart') || [];
   return items.length;
+}
+
+export function quantHandler(event){
+  const itemId = event.target.dataset.id;
+  const quant = event.target.value;
+  const cartItems = getLocalStorage('so=cart') || [];
+  const currentProduct = cartItems.find((item) => item.Id == itemId);
+  currentProduct.currentQuantity = quant;
+  setLocalStorage('so-cart', cartItems);
+}
+
+export function quantityItem(event){
+  // var quant = countItem()
+
+  var quant = event.target;
+  quant = event.currentQuantity;
+  // eslint-disable-next-line no-console
+  console.log(event);
+
+  return (parseInt(quant) * event.FinalPrice); 
 }
